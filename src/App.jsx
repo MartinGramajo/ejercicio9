fa8b8991c380ca049fb1a5b7bf6cfaa12f92e651
¨mago¨ <martingramajo08@gmail.com>
2023-07-21 22:31:32 -0300

    agrego el mapeo y el component CardCita

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import FormularioPacientes from "./components/FormularioPacientes";
+import CardCita from "./components/CardCita";
 
 function App() {
   let citasIniciales = JSON.parse(localStorage.getItem("citas"));
@@ -23,10 +24,25 @@ function App() {
   return (
     <div className="bg-app d-flex flex-column min-vh-100">
       <section>
-        <h1 className="text-center py-5">Administrador de Citas</h1>
+        <h1 className="text-center py-5 display-6">
+          Administrador pacientes de veterinaria
+        </h1>
       </section>
       <section>
         <FormularioPacientes nuevaCita={nuevaCita} />
+      </section>
+      <section>
+        <div className="caja py-5 text-center">
+          {citas.lenght === 0 ? (
+            "no hay citas"
+          ) : (
+            <div className="container d-flex flex-wrap justify-content-between">
+              {citas.map((cita) => (
+                <CardCita key={cita.id} cita={cita} />
+              ))}
+            </div>
+          )}
+        </div>
       </section>
       <footer className="py-5 mt-auto bg-dark">
         <h5 className="text-center text-white">
---
+++ src/components/CardCita.jsx
@@ -0,0 +1,27 @@
+import { Button, Card } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserMinus } from "@fortawesome/free-solid-svg-icons";
+
+export default function CardCita({ cita }) {
+  return (
+    <Card className="container my-5 cita-carta">
+      <Card.Header className="d-flex py-4">
+        <div className="circulo"></div>
+        <div className="ms-4">
+          <h6>Mascota: {cita.mascota}</h6>
+          <h6 className="text-black-50">Dueño: {cita.propietario}</h6>
+        </div>
+      </Card.Header>
+      <Card.Body className="py-5 bg-celeste">
+        <h6>Fecha: {cita.fecha}</h6>
+        <h6>Hora: {cita.hora}</h6>
+        <h6>Síntomas:{cita.sintomas}</h6>
+      </Card.Body>
+      <Card.Footer className="d-flex justify-content-end py-4">
+        <Button variant="danger" size="sm">
+          <FontAwesomeIcon icon={faUserMinus} /> Borrar
+        </Button>
+      </Card.Footer>
+    </Card>
+  );
+}
