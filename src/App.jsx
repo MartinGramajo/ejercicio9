9e626c5bf7b36b0e65fe0b138544e77c61cd321d
¨mago¨ <martingramajo08@gmail.com>
2023-07-21 22:13:56 -0300

    en app creo los state para las citas iniciales y agrego el localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import FormularioPacientes from "./components/FormularioPacientes";
+
 function App() {
+  let citasIniciales = JSON.parse(localStorage.getItem("citas"));
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
+  const [citas, setCitas] = useState(citasIniciales);
+  useEffect(() => {
+    if (citasIniciales) {
+      localStorage.setItem("citas", JSON.stringify(citas));
+    } else {
+      localStorage.setItem("citas", JSON.stringify([]));
+    }
+  }, [citas]);
+
+  const nuevaCita = (cita) => {
+    setCitas([...citas, cita]);
+  };
+
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className="bg-app d-flex flex-column min-vh-100">
+      <section>
+        <h1 className="text-center py-5">Administrador de Citas</h1>
+      </section>
+      <section>
+        <FormularioPacientes nuevaCita={nuevaCita} />
+      </section>
+      <footer className="py-5 mt-auto bg-dark">
+        <h5 className="text-center text-white">
+          {" "}
+          © Todos los derechos reservados
+        </h5>
+      </footer>
+    </div>
   );
 }
 
